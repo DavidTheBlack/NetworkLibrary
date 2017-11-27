f1c52bc43104773675ed1731b2e4a992aa799b02
David <dgaldo@ict-solutions.it>
2017-11-27 20:16:03 +0100

    Messages changed

--- NetworkLibraryMessages/NetMessages.cs
+++ NetworkLibraryMessages/NetMessages.cs
@@ -4,26 +4,45 @@ namespace NetworkLibraryMessages
 {
     static public class NetMessages
     {
-        /*Protocol Messages*/
+        /*Application Protocol Messages*/
+        //[packet Header][Payload Type]|[data1]|...|[dataN]|[packet Trailer]
+        //
+        //Data packet
+        //<D|user1|user2|user3|groundAccX|groundAccY|groundAccZ>
+
+        // Protocol Information packet
+        //<P|[message]>
+
+            
+
+        /// <summary>
+        /// Header used for sending package
+        /// </summary>
+        public static string packetHeader = "<";
+
+        /// <summary>
+        /// header used for close a message
+        /// </summary>
+        public static string packetTrailer = ">";
+
+        /// <summary>
+        /// header used for sending data (user position and table acceleration
+        /// </summary>
+        public static string dataPayload = "D";
 
         /// <summary>
         /// Header used for sending protocol message
         /// </summary>
-        public static string messageHeader = "<M>";
-        /// <summary>
-        /// header used for sending User position
-        /// </summary>
-        public static string userPositionHeader = "<U>";
-        /// <summary>
-        /// Header used for sending ground acceleration
-        /// </summary>
-        public static string accelerationHeader = "<A>";
-        //header used for close a message
-        public static string messageTrailer = "<E>";
+        public static string protocolHeader = "P";
+
+
+
+        
         /// <summary>
         /// Header used to comunicate the starting of the experiment
         /// </summary>
         public static string startSignal = "Start";
+
         /// <summary>
         /// Header used to comunicate the ending of the experiment
         /// </summary>
@@ -31,20 +50,26 @@ namespace NetworkLibraryMessages
 
         //data separator
         public static string token = "|";
+
         //Message sent to accept a Conection
         public static string connectionAccepted = "ConnectionAccepted";
-        //Message sent for comunicate that the endpoint is ready to receive messages
-        public static string readyToReceive = "ReadyToRec";
+
         //Message sent to comunicate the endpoint disconnection
         public static string disconnectRequest =  "Disconnect";
+
         //Message sent to comunicate that the player is lost
         public static string playerIsLost = "PlayerLost";
-        //Communicate to load the indoor envinronment
-        public static string loadIndoor = "LoadIndoor";
-        //Communicate to load the outdoor envinronment
-        public static string loadOutdoor = "LoadOutdoor";
+
         //Communicate to load the intro envinronment
         public static string loadIntro = "LoadIntro";
+
+        //Communicate to load the outdoor envinronment
+        public static string loadOutdoor = "LoadOutdoor";
+
+        //Communicate to load the indoor envinronment
+        public static string loadIndoor = "LoadIndoor";
+
+
 
 
     }
